1ba93dc5574a3958a1bf36fc039e61276482fdfe
Elena7110 <77455685+Elena7110@users.noreply.github.com>
2021-04-20 19:40:14 +0300

    Beauty Salon Tea

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$('.slider__hair').slick({
+		autoplay:true,
+		autoplaySpeed:1000,
+		pauseOnHover:false,
+		touchThreshold:10,
+		waitForAnimate:false
+	});
+	$('.slider__nail').slick({
+		autoplay:true,
+		autoplaySpeed:1000,
+		pauseOnHover:false,
+		touchThreshold:10,
+		waitForAnimate:false
+	});
+	$('.slider__cosm').slick({
+		autoplay:true,
+		autoplaySpeed:1000,
+		pauseOnHover:false,
+		touchThreshold:10,
+		waitForAnimate:false
+	});
+});
